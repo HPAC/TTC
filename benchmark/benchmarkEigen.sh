1f5025f40b896f144985c13459a3f9d20722f045
Paul Springer <springer@aices.rwth-aachen.de>
2017-04-10 10:03:28 +0200

    eigen script added

---
+++ benchmark/benchmarkEigen.sh
@@ -0,0 +1,115 @@
+rm -f eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen0.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen1.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen2.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen3.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen4.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen5.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen6.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen7.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen8.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen9.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen10.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen11.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen12.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen13.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen14.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen15.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen16.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen17.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen18.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen19.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen20.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen21.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen22.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen23.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen24.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen25.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen26.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen27.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen28.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen29.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen30.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen31.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen32.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen33.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen34.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen35.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen36.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen37.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen38.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen39.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen40.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen41.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen42.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen43.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen44.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen45.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen46.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen47.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen48.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen49.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen50.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen51.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen52.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen53.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen54.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen55.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
+icpc -O3 -I/home/<PATH TO EIGEN>/ -std=c++14 -qopenmp -xHost eigen56.cpp
+KMP_AFFINITY=compact,1 OMP_NUM_THREADS=24 numactl --interleave=all ./a.out >>eigen.dat
